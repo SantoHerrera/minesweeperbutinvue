4320ee031def12e0b9c525e36abeaa1970a24a2b
Rafael Herrera <santoElias65537@gmail.com>
2020-04-07 18:09:48 -0600

    lost asf

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from "./components/HelloWorld.vue";
+import Vue from 'vue'
+import App from './components/App/App';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+
+new Vue({
+  el: "#app",
+  render: h => h(App),
+  data: {
+    sales: [
+      [
+        { Year: 2018, Sale: 512 },
+        { Year: 2018, Sale: 1025 },
+        { Year: 2018, Sale: 70 }
+      ],
+      [
+        { Year: 2017, Sale: 155 },
+        { Year: 2017, Sale: 12 },
+        { Year: 2017, Sale: 69 }
+      ],
+      [
+        { Year: 2016, Sale: 8767 },
+        { Year: 2016, Sale: 29 },
+        { Year: 2016, Sale: 72 }
+      ]
+    ]
+  },
+  methods: {
+    clickThatMothafucka: function(some) {
+      console.log("shit was clicked", some);
+    }
   }
-}
-</script>
+});
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -5,4 +5,55 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  data: {
+    sales: [
+      [
+        { Year: 2018, Sale: 512 },
+        { Year: 2018, Sale: 1025 },
+        { Year: 2018, Sale: 70 }
+      ],
+      [
+        { Year: 2017, Sale: 155 },
+        { Year: 2017, Sale: 12 },
+        { Year: 2017, Sale: 69 }
+      ],
+      [
+        { Year: 2016, Sale: 8767 },
+        { Year: 2016, Sale: 29 },
+        { Year: 2016, Sale: 72 }
+      ]
+    ]
+  }
 }).$mount('#app')
+
+/*
+new Vue({
+  el: "#app",
+  render: h => h(App),
+  data: {
+    sales: [
+      [
+        { Year: 2018, Sale: 512 },
+        { Year: 2018, Sale: 1025 },
+        { Year: 2018, Sale: 70 }
+      ],
+      [
+        { Year: 2017, Sale: 155 },
+        { Year: 2017, Sale: 12 },
+        { Year: 2017, Sale: 69 }
+      ],
+      [
+        { Year: 2016, Sale: 8767 },
+        { Year: 2016, Sale: 29 },
+        { Year: 2016, Sale: 72 }
+      ]
+    ]
+  },
+  methods: {
+    clickThatMothafucka: function(some) {
+      console.log("shit was clicked", some);
+    }
+  }
+});
+
+*/
