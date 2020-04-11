741aa42596b40a72de78a4b5b195aa7f33ca8356
Rafael Herrera <santoElias65537@gmail.com>
2020-04-11 12:38:01 -0600

    forgot what I did, but I did something

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,8 +2,13 @@
   <div class="hello">
     <table>
       <tbody>
-        <tr v-for="(sale,i) in sales" :key="i">
-          <td v-for="(cell, j) in sale" :key="i-j">{{ cell.Sale }}</td>
+        <tr v-for="(row,i) in board" :key="i">
+          <td
+            v-for="(cell, j) in row"
+            :key="`${i}-${j}`"
+            :id="`${i}-${j}`"
+            v-on:click="rightClick"
+          >{{ cell.screen }} {{cell.hasbomb }}</td>
         </tr>
       </tbody>
     </table>
@@ -14,29 +19,110 @@
 <script>
 export default {
   name: "HelloWorld",
-  data() {
+  data: function() {
     return {
-      sales: [
-        [
-          { Year: 2018, Sale: 512 },
-          { Year: 2018, Sale: 1025 },
-          { Year: 2018, Sale: 70 }
-        ],
-        [
-          { Year: 2017, Sale: 155 },
-          { Year: 2017, Sale: 12 },
-          { Year: 2017, Sale: 69 }
-        ],
-        [
-          { Year: 2016, Sale: 8767 },
-          { Year: 2016, Sale: 29 },
-          { Year: 2016, Sale: 72 }
-        ]
-      ],
-    shitLinked: 'Please appear'
+      shitLinked: "Please appear",
+      board: this.createNestedArray(9, 6),
+      firstClick: true
+    };
+  },
+  methods: {
+    randomTrueFalse: function() {
+      return Math.random() <= 0.2;
+    },
+    createNestedArray: function(x, y) {
+      let nestedArray = [];
+      for (let i = 0; i < y; i++) {
+        nestedArray.push([]);
+        for (let j = 0; j < x; j++) {
+          //true represents bomb
+          let hasbomb = this.randomTrueFalse();
+          //information of individual cell
+          nestedArray[i][j] = {
+            screen: "?",
+            hasbomb: hasbomb,
+            hasBeenClicked: false,
+            nearbyBombs: 0,
+            id: `${j}-${i}`,
+            wasrightClicked: false
+          };
+        }
+      }
+      return nestedArray;
+    },
+    stringToId: function(string) {
+      //given 'x-y' will return ['x', 'y']
+      let theId = string.split("-");
+      //x && y will be turned into numbers, returns array
+      let ID = theId.map(stringNumber => parseInt(stringNumber));
+      return ID;
+    },
+    rightClick: function(cell) {
+      //let Id = this.stringToId(cell.target.id);
+
+      let cellClicked = this.getCell(cell.target.id);
+
+      if (this.firstClick) {
+        //this.fixFirstClickBomb(cell.target.id);
+        this.firstClick = false;
+
+        this.allNearbyCells(cellClicked.id, cell => {
+          console.log(cell.id);
+        });
+      }
+
+      if (cellClicked.hasbomb) {
+        alert("youve clicked a bomb, reload page to play again");
+      }
+    },
+    fixFirstClickBomb: function() {
+      //let bombsRemoved = [];
+    },
+    removeBomb: function(cellId) {
+      this.getCell(cellId, cell => {
+        cell.hasBomb = false;
+      });
+    },
+    getCell: function(cellID, fn) {
+      //modify one cell
+      let Id = this.stringToId(cellID);
+
+      if (fn) {
+        fn(this.board[Id[0]][Id[1]]);
+      }
+      return this.board[Id[0]][Id[1]];
+    },
+    allNearbyCells: function(cellId, fn) {
+      let Id = this.stringToId(cellId);
+
+      const x = Id[0];
+      const y = Id[1];
+
+      for (let i = x - 1; i <= x + 1; i++) {
+        for (let j = y - 1; j <= y + 1; j++) {
+          //if it dont exist, move on
+          if (!this.board[i] || !this.board[i][j]) {
+            continue;
+          }
+          //dont put the box you clicked on the array of ids that represent the surrounding ids
+          if (i === x && j === y) {
+            continue;
+          }
+
+          fn(this.board[i][j]);
+        }
+      }
     }
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+td,
+th {
+  border: 1px solid black;
+}
+</style>>
+
+
